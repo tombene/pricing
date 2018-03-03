ed1d3a659cc9169d2ed88052bc3c4c903d5dedb9
Thomas Benedict <tomtombene@gmail.com>
2018-03-03 13:01:35 -0700

    walmartapi stuff

--- assets/js/walmartapi.js
+++ assets/js/walmartapi.js
@@ -4,14 +4,24 @@ var wm = {
 	description: '',
 	name: '',
 	upc: '',
-	image: '',
+	image: ['','','','',''],
 	url: '',
 	getItems: function(searchTerm,renderCallBack){
-		queryURL = '';
+		queryURL = 'http://api.walmartlabs.com/v1/search?query='+ searchTerm +'&format=json&apiKey=ke74j6r2h3zd93y27veta57j';
 		$.ajax({
-
+			url: queryURL,
+			method: "GET"
 		}).then(function(response){
-
+			var i = 0;
+			wm.price = response.items[i].salePrice;
+			wm.model = response.items[i].modelNumber;
+			wm.description = response.items[i].shortDescription;
+			wm.name = response.items[i].name;
+			wm.upc = response.items[i].upc;
+			for(var j;j<5;j++){
+				
+			}
+			wm.image = response.items[i].image
 		});
 
 	}
