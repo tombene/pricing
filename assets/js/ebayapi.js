35eea2917942d727cc139adbb1decb7adc400328
Tyler Schmauch <tschmauch@hotmail.com>
2018-03-06 21:19:15 -0700

    catching up

--- assets/js/ebayapi.js
+++ assets/js/ebayapi.js
@@ -9,20 +9,29 @@ var ebay = {
 		url: ''
 	},
 	getItems: function (searchTerm, renderCallback) {
-		queryURL = 'http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=TylerSch-pricingC-PRD-edf6c4a56-cf9e2e63&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=' + searchTerm + '&paginationInput.entriesPerPage=5';
+		searchTerm = 'baseball bat';
+		queryURL = 'http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=TylerSch-pricingC-PRD-edf6c4a56-cf9e2e63&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=' + searchTerm + '&paginationInput.entriesPerPage=1';
 		$.ajax({
 			url: queryURL,
 			method: "GET"
 		}).then(function (response) {
 			var i = 0;
-			ebay.currentItem.price = response.items[i].salePrice;
-			ebay.currentItem.model = response.items[i].modelNumber;
-			ebay.currentItem.description = response.items[i].shortDescription;
-			ebay.currentItem.name = response.items[i].name;
-			ebay.currentItem.upc = response.items[i].upc;
-			for (var j; j < 5; j++) {
-				ebay.currentItem.image[j] = response.items[i].imageEntities[j].thumbnailImage;
+			console.log(JSON.parse(response));
+			var data = JSON.parse(response);
+			ebay.currentItem.price = data.findItemsByKeywordsResponse[i].searchResult[0].item[0].sellingStatus[0].currentPrice[0].__value__;
+			ebay.currentItem.model = data.findItemsByKeywordsResponse[i].searchResult[0].item[0].itemId[0];
+			ebay.currentItem.description = data.findItemsByKeywordsResponse[i].searchResult[0].item[0].sellingStatus[0].currentPrice[0].__value__;
+			ebay.currentItem.name = data.findItemsByKeywordsResponse[i].searchResult[0].item[0].title[0];
+			ebay.currentItem.upc = data.findItemsByKeywordsResponse[i].searchResult[0].item[0].itemId[0];
+			// for (var j; j < 5; j++) {
+			// 	ebay.currentItem.image[j] = data.findItemsByKeywordsResponse[i].searchResult[0].item[0].sellingStatus[0].currentPrice[0].__value__;
+				ebay.currentItem.url = data.findItemsByKeywordsResponse[i].searchResult[0].item[0].viewItemURL[0];
+				console.log(ebay.currentItem);
 			}
-			renderCallBack(ebay.currentItem);
-		}
+			// renderCallBack(ebay.currentItem, 'ebay');
+		)}
+	}
+
+
+ebay.getItems();
 
