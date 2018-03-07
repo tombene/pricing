e8c4704e5429abaa06b769c70dc3967caf6e1377
Tyler Schmauch <tschmauch@hotmail.com>
2018-03-06 18:36:26 -0700

    first of day

--- assets/js/ebayapi.js
+++ assets/js/ebayapi.js
@@ -6,11 +6,11 @@ var ebay = {
 	upc: '',
 	image: '',
 	link: '',
-	queryURL: 'http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=TylerSch-pricingC-PRD-edf6c4a56-cf9e2e63&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=' + searchTerm
-	getItem: function(searchTerm) {
+	queryURL: 'http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=TylerSch-pricingC-PRD-edf6c4a56-cf9e2e63&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=' + encodeURI(searchTerm)
+	getItem: function(searchTerm, renderCallback) {
 		$.ajax({
 			url: this.queryURL,
-			method: "findItemsByKeywords"
+			method: "GET"
 		}).then(function (renderFunction(response)))
 	}
 }
