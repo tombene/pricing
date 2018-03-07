fe83b522b498c024c74517fd4648abf3d7b0ce6c
Thomas Benedict <tomtombene@gmail.com>
2018-03-06 19:17:28 -0700

    walmart api stuff

--- assets/js/walmartapi.js
+++ assets/js/walmartapi.js
@@ -1,11 +1,13 @@
-var wm = {
-	price: 0,
-	model: '',
-	description: '',
-	name: '',
-	upc: '',
-	image: ['','','','',''],
-	url: '',
+var walmart = {
+	currentItem: {
+		price: 0,
+		model: '',
+		description: '',
+		name: '',
+		upc: '',
+		image: ['','','','',''],
+		url: ''
+	},
 	getItems: function(searchTerm,renderCallBack){
 		queryURL = 'http://api.walmartlabs.com/v1/search?query='+ searchTerm +'&format=json&apiKey=ke74j6r2h3zd93y27veta57j';
 		$.ajax({
@@ -13,16 +15,15 @@ var wm = {
 			method: "GET"
 		}).then(function(response){
 			var i = 0;
-			wm.price = response.items[i].salePrice;
-			wm.model = response.items[i].modelNumber;
-			wm.description = response.items[i].shortDescription;
-			wm.name = response.items[i].name;
-			wm.upc = response.items[i].upc;
+			walmart.currentItem.price = response.items[i].salePrice;
+			walmart.currentItem.model = response.items[i].modelNumber;
+			walmart.currentItem.description = response.items[i].shortDescription;
+			walmart.currentItem.name = response.items[i].name;
+			walmart.currentItem.upc = response.items[i].upc;
 			for(var j;j<5;j++){
-				
+				walmart.currentItem.image[j] = response.items[i].imageEntities[j].thumbnailImage;
 			}
-			wm.image = response.items[i].image
+			renderCallBack(walmart.currentItem);
 		});
-
 	}
 }
