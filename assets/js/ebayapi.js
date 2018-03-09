fe9a447d9f11d0833f3bc051bf4268a48f3647a2
Csgraser <csgraser@gmail.com>
2018-03-08 18:50:02 -0700

    added https instead of http

--- assets/js/ebayapi.js
+++ assets/js/ebayapi.js
@@ -6,7 +6,7 @@ var ebay = {
 	upc: '',
 	image: '',
 	link: '',
-	queryURL: 'http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=TylerSch-pricingC-PRD-edf6c4a56-cf9e2e63&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=' + searchTerm
+	queryURL: 'https://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=TylerSch-pricingC-PRD-edf6c4a56-cf9e2e63&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=' + searchTerm
 	getItem: function(searchTerm) {
 		$.ajax({
 			url: this.queryURL,
