11830aa4df497d81100936b8da95d4d50e73f6d4
Csgraser <csgraser@gmail.com>
2018-03-13 21:17:51 -0600

    working on it

--- assets/js/discountapi.js
+++ assets/js/discountapi.js
@@ -11,7 +11,7 @@ var discount = {
 			method: "GET",
 		}).then(function (response) {
 			console.log(response);
-			$("#discount-container").append("<p>" + 'Check out this related coupon! - ' + response.deals[0].deal.merchant.name + "</p>");
+			$("#discount-container").append("<p>" + 'Check out this (somewhat) related coupon! - ' + response.deals[0].deal.merchant.name + "</p>");
 			$("#discount-container").append("<a href='" + response.deals[0].deal.url + "'><img src='" + response.deals[0].deal.image_url + "' /> </a>");
 			
 			if (searchTerm === ""){
